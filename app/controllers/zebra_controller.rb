327b2aeb0bd5a369b4fefa590d4ec57b4406f3d2
nhatminhdao <nhatminhdao@gmail.com>
2024-07-27 16:19:05 +0000

    final

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,11 +1,48 @@
 class ZebraController < ApplicationController
-  def giraffe
-    @random_move = ["rock", "paper", "scissors"].sample
+  def rules
+      render({ :template => "game_templates/rules" })
+  end
+  
+  def rock
+    moves = ["rock", "paper", "scissors"]
+    @computer_move = moves.sample
+    
+    if @computer_move == "rock"
+      @outcome = "tied"
+    elsif @computer_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    
     render({ :template => "game_templates/play_rock" })
   end
 
-  def elephant
-    @random_move = ["rock", "paper", "scissors"].sample
+  def paper
+    moves = ["rock", "paper", "scissors"]
+    @computer_move = moves.sample
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
+
     render({ :template => "game_templates/play_paper" })
+  end
+
+  def scissors
+    moves = ["rock", "paper", "scissors"]
+    @computer_move = moves.sample
+    if @computer_move == "rock"
+      @outcome = "lost"
+    elsif @computer_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/play_scissors" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get("/rock", { :controller => "zebra", :action => "giraffe" })
+  get("/", { :controller => "zebra", :action => "rules" })
+  
+  get("/rock", { :controller => "zebra", :action => "rock" })
 
-  get("/paper", { :controller => "zebra", :action => "elephant" })
+  get("/paper", { :controller => "zebra", :action => "paper" })
 
-  get("/scissors", { :controller => "zebra", :action => "lion" })
+  get("/scissors", { :controller => "zebra", :action => "scissors" }) 
   end
